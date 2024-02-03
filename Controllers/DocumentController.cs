8f128d9071c73589f17bec97bb61574bb14342e0
Sergio M <sergiomorenomartinez544@gmail.com>
2024-02-03 12:29:39 +0100

    Added controllers

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -12,22 +12,12 @@ public class DocumentController : BaseController {
         return _database.GetDocDetails(docIds);
     }
     
-    /// <summary>
-    /// Retrieves the words by Id
-    /// </summary>
-    /// <param name="wordIds"></param>
-    /// <returns></returns>
     [HttpGet("GetByWordIds")]
     public Dictionary<int, int> GetByWordIds([FromQuery] List<int> wordIds)
     {
         return _database.GetDocuments(wordIds);
     }
-
-    /// <summary>
-    /// Inserts a document
-    /// </summary>
-    /// <param name="id"></param>
-    /// <param name="url"></param>
+    
     [HttpPost]
     public void Post(int id, string url)
     {
